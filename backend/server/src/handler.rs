5d4feee20578b41a19b1c204f313213539688006
ohayo <ddmmy@hotmail.com>
2026-04-18 09:13:24 +0800

    add check session request authorized

--- backend/server/src/handler.rs
+++ backend/server/src/handler.rs
@@ -4,7 +4,7 @@ use axum::response::IntoResponse;
 use serde::Deserialize;
 use crate::AppState;
 use crate::error::ApiResult;
-use crate::extractor::DbConnection;
+use crate::extractor::{DbConnection, UserSession};
 
 pub mod user;
 
@@ -27,4 +27,28 @@ pub async fn with_public_handler<'a, Req>(
 where Req:PublicApiRequest + Deserialize<'a>
 {
     payload.process_request(conn, state).await
+}
+
+// 会话请求认证
+#[async_trait]
+pub trait AuthorizatedApiRequest {
+    type Response: IntoResponse;
+
+    async fn process_request(
+        self,
+        conn: DbConnection,
+        session: UserSession,
+        state: AppState,
+    ) -> ApiResult<Self::Response>;
+}
+
+pub async fn with_handler<'a, Req>(
+    conn: DbConnection,
+    session: UserSession,
+    State(state): State<AppState>,
+    Json(payload): Json<Req>,
+) -> ApiResult<Req::Response>
+where Req:AuthorizatedApiRequest + Deserialize<'a>
+{
+    payload.process_request(conn, session, state).await
 }
